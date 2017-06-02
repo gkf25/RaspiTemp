69445c6d2e177fdb5bcc16e65e9350d71cdc8b7a
gkf <guitarkif@free.fr>
2017-06-02 15:44:10 +0200

    Ajout de commentaires dans PiTempAlerte

--- PiTempAlerte.py
+++ PiTempAlerte.py
@@ -8,8 +8,12 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import subprocess
 
+
+#definition des fonctions 
+
 def eteindre():
-    """il est necessaire d'editer le fichier 'sudoers' à l'aide de la commande:
+    """Fonction permettant l'extinction du raspberry
+    il est necessaire d'editer le fichier 'sudoers' à l'aide de la commande:
     sudo visudo
     operator ALL=/sbin/shutdown
     operator ALL=NOPASSWD: /sbin/shutdown"""
@@ -20,6 +24,7 @@ def eteindre():
 
 
 def mail(message):
+	"""Parametre necessaire à l'envoi et la reception de mails pour etre informé de l'alerte  emise"""
     toaddr = "franck.mottas@gmail.com"
     fromaddr = "fmottas@parkeon.com"
     msg = MIMEMultipart()
@@ -34,21 +39,25 @@ def mail(message):
     text = msg.as_string()
     server.sendmail(fromaddr, toaddr, text)
     server.quit()
-    
+
+# recuperation de la valeur de la temperature    
 with open(r'/sys/class/thermal/thermal_zone0/temp', 'r') as f:
     t = f.readlines()
 
+# formattage de la temperature et de l'heure
 temp = round(int(t[0])/1000,2)
 date = datetime.datetime.now().strftime("%d-%m-%Y")
 heure = datetime.datetime.now().strftime("%H:%M:%S")
-path_dest =  r'/home/gkf/Documents/PiTemp/donnee'
 
+# copie des données formatées dans le fichier defini par le path
+path_dest =  r'/home/gkf/Documents/PiTemp/donnee'
 with open(os.path.join(path_dest,date),'a') as f:
     f.write(str(temp))
     f.write("   ,   ")
     f.write(heure)
     f.write("\n")
 
+# definition des diferents seuils et de leurs actions respective
 if temp > 75: 
    avertissement = "La température a dépassée le seuil limite de 75°C, le raspberry va s'éteindre !"
    mail(avertissement)   
